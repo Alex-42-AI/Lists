f8d2b8bedf7eac07214145ede2844e901e356a34
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-09-04 03:11:23 +0300

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -2,11 +2,8 @@ class SortedList:
     def __init__(self, f=lambda x: x):
         self.__f = f
         self.__value = []
-    def value(self):
-        return self.__value
-    def pop(self, index: int = -1):
-        res = self.__value.pop(index)
-        return res
+    def value(self): return self.__value
+    def pop(self, index: int = -1): return self.__value.pop(index)
     def insert(self, el):
         low, high = 0, len(self)
         while low < high:
@@ -14,11 +11,9 @@ class SortedList:
             if self.__f(el) == self.__f(self[mid]):
                 self.__value.insert(mid, el)
                 return
-            if self.__f(el) < self.__f(self[mid]):
-                high = mid
+            if self.__f(el) < self.__f(self[mid]): high = mid
             else:
-                if low == mid + 1:
-                    break
+                if low == mid + 1: break
                 low = mid + 1
         self.__value.insert(high, el)
     def remove(self, el):
@@ -45,72 +40,55 @@ class SortedList:
                         j += 1
                         still = True
                 return False
-            if self.__f(el) < self.__f(self[mid]):
-                high = mid
+            if self.__f(el) < self.__f(self[mid]): high = mid
             else:
-                if low == mid:
-                    return False
+                if low == mid: return False
                 low = mid
     def copy(self):
         res = SortedList(self.__f)
-        for el in self:
-            res.insert(el)
+        for el in self: res.insert(el)
         return res
     def merge(self, other):
         if isinstance(other, SortedList):
-            if any([self.__f(el) != other.__f(el) for el in self.__value + other.__value]):
-                raise ValueError("Sorting functions of both lists are different!")
+            if any([self.__f(el) != other.__f(el) for el in self.__value + other.__value]): raise ValueError("Sorting functions of both lists are different!")
             res = self.copy()
-            for el in other:
-                res.insert(el)
+            for el in other: res.insert(el)
             return res
-    def __len__(self):
-        return len(self.__value)
+    def __len__(self): return len(self.__value)
     def __contains__(self, item):
         low, high = 0, len(self)
         while low < high:
             mid = (low + high) // 2
             if self.__f(item) == self.__f(self[mid]):
-                if item == self[mid]:
-                    return True
+                if item == self[mid]: return True
                 i, j = mid - 1, mid + 1
                 while True:
                     if i >= 0 and self.__f(self[i]) == self.__f(item):
-                        if item == self[i]:
-                            return True
+                        if item == self[i]: return True
                         i -= 1
                         continue
                     if j < len(self) and self.__f(self[j]) == self.__f(item):
-                        if item == self[j]:
-                            return True
+                        if item == self[j]: return True
                         j += 1
                         continue
                     break
                 return False
-            if self.__f(item) < self.__f(self[mid]):
-                high = mid
+            if self.__f(item) < self.__f(self[mid]): high = mid
             else:
-                if low == mid:
-                    return False
+                if low == mid: return False
                 low = mid
-    def __bool__(self):
-        return bool(self.__value)
+    def __bool__(self): return bool(self.__value)
     def __getitem__(self, item: int | slice):
         if isinstance(item, slice):
             res = SortedList(self.__f)
             res.__value = self.__value[item]
             return res
         return self.__value[item]
-    def __setitem__(self, i: int, value):
-        self.remove(self[i]), self.insert(value)
-    def __add__(self, other):
-        return self.merge(other)
-    def __eq__(self, other):
-        return self.__value == other
-    def __str__(self):
-        return str(self.__value)
-    def __repr__(self):
-        return str(self)
+    def __setitem__(self, i: int, value): self.remove(self[i]), self.insert(value)
+    def __add__(self, other): return self.merge(other)
+    def __eq__(self, other): return self.__value == other
+    def __str__(self): return str(self.__value)
+    def __repr__(self): return str(self)
 def original(data: [int], starter: int = 0, jump: int = 1):
     for i in range(len(data)):
         if data[i] > i: raise ValueError
