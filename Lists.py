129eb215b6fbf69a65286030ebcb42c28813dfab
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-09-04 03:07:22 +0300

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -2,7 +2,8 @@ class SortedList:
     def __init__(self, f=lambda x: x):
         self.__f = f
         self.__value = []
-        self.__i = -1
+    def value(self):
+        return self.__value
     def pop(self, index: int = -1):
         res = self.__value.pop(index)
         return res
@@ -55,6 +56,14 @@ class SortedList:
         for el in self:
             res.insert(el)
         return res
+    def merge(self, other):
+        if isinstance(other, SortedList):
+            if any([self.__f(el) != other.__f(el) for el in self.__value + other.__value]):
+                raise ValueError("Sorting functions of both lists are different!")
+            res = self.copy()
+            for el in other:
+                res.insert(el)
+            return res
     def __len__(self):
         return len(self.__value)
     def __contains__(self, item):
@@ -86,14 +95,6 @@ class SortedList:
                 low = mid
     def __bool__(self):
         return bool(self.__value)
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.__i + 1 == len(self):
-            self.__i = -1
-            raise StopIteration()
-        self.__i += 1
-        return self[self.__i]
     def __getitem__(self, item: int | slice):
         if isinstance(item, slice):
             res = SortedList(self.__f)
@@ -103,13 +104,7 @@ class SortedList:
     def __setitem__(self, i: int, value):
         self.remove(self[i]), self.insert(value)
     def __add__(self, other):
-        if isinstance(other, SortedList):
-            if any([self.__f(el) != other.__f(el) for el in self.__value + other.__value]):
-                raise ValueError("Sorting functions of both lists are different!")
-            res = self.copy()
-            for el in other:
-                res.insert(el)
-            return res
+        return self.merge(other)
     def __eq__(self, other):
         return self.__value == other
     def __str__(self):
@@ -118,22 +113,19 @@ class SortedList:
         return str(self)
 def original(data: [int], starter: int = 0, jump: int = 1):
     for i in range(len(data)):
-        if data[i] > i:
-            raise ValueError
+        if data[i] > i: raise ValueError
     sorted_res = []
     for index in range(len(data)):
         curr = index
         curr -= data[index]
         sorted_res.insert(curr, index)
     res = [0] * len(data)
-    for index in range(len(sorted_res)):
-        res[sorted_res[index]] = index * jump + starter
+    for index in range(len(sorted_res)): res[sorted_res[index]] = index * jump + starter
     return res
 def bubble_sort(array: iter):
     for i in range(len(array)):
         for j in range(len(array[i + 1:])):
-            if array[i] > array[j]:
-                array[i], array[j] = array[j], array[i]
+            if array[i] > array[j]: array[i], array[j] = array[j], array[i]
 def partition(array: iter, low: int, high: int):
     pivot, i = array[high], low - 1
     for j in range(low, high):
@@ -143,31 +135,25 @@ def partition(array: iter, low: int, high: int):
     array[i + 1], array[high] = pivot, array[i + 1]
     return i + 1
 def quick_sort(array: iter, low=0, high=-1):
-    if high == -1:
-        high = len(array) - 1
+    if high == -1: high = len(array) - 1
     if low < high:
         pivot = partition(array, low, high)
         quick_sort(array, low, pivot - 1), quick_sort(array, pivot + 1, high)
 def is_sorted(l: [float]) -> bool:
     for i in range(0, len(l) - 1):
-        if l[i] > l[i + 1]:
-            return False
+        if l[i] > l[i + 1]: return False
     return True
 def heapify(ll: [int], l: int, h: int, i: int, f=max):
     left, right = l + 2 * (i - l), l + 2 * (i - l) + 1
     res = i
-    if l + left <= h and ll[l + i - 1] != f(ll[l + left - 1], ll[l + i - 1]):
-        res = left
-    if l + right <= h and ll[l + res - 1] != f(ll[l + right - 1], ll[l + res - 1]):
-        res = right
+    if l + left <= h and ll[l + i - 1] != f(ll[l + left - 1], ll[l + i - 1]): res = left
+    if l + right <= h and ll[l + res - 1] != f(ll[l + right - 1], ll[l + res - 1]): res = right
     if res != i:
         ll[l + i - 1], ll[l + res - 1] = ll[l + res - 1], ll[l + i - 1]
         heapify(ll, l, h, res, f)
 def build_heap(ll: [int], h: int = 0):
-    if not h:
-        h = len(ll)
-    for i in range(h // 2, 2, -1):
-        heapify(ll, 2, h, i)
+    if not h: h = len(ll)
+    for i in range(h // 2, 2, -1): heapify(ll, 2, h, i)
 def heapsort(ll: [int]):
     build_heap(ll)
     h = len(ll)
@@ -193,8 +179,7 @@ def merge_sort(A: [float]):
                 A[k] = L[j]
                 k += 1
     def helper(l, h):
-        if l >= h:
-            return
+        if l >= h: return
         mid = (h + l) // 2
         helper(l, mid)
         helper(mid + 1, h)
